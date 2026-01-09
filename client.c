cd61d82632907d573e887e954f145db2f0670d09
Brian Zheng <brianz76@nycstudents.net>
2026-01-09 15:15:01 -0500

    start select logic for client

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 #include "networking.h"
-
+#include <sys/select.h>
 
 struct message {
   char msg[1025];
@@ -7,4 +7,49 @@ struct message {
   char time[9];
 };
 
-struct message messages[100];
+
+int main() {
+  struct message messages[100];
+
+  int server_socket;
+  fd_set read_fds;
+
+  server_socket = client_tcp_handshake("127.0.0.1");
+
+
+  while (1) {
+    FD_ZERO(&read_fds);
+
+    FD_SET(STDIN_FILENO, &read_fds);
+    FD_SET(server_socket, &read_fds);
+
+    select(server_socket + 1, &read_fds, NULL, NULL, NULL);
+
+    if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+      char buf[1025];
+
+      int bytes = fgets(buf, sizeof(buf), stdin);
+      err(bytes, "read from stdin");
+
+      bytes = write(server_socket, buf, sizeof(buf));
+      err(bytes, "write message to server");
+
+
+
+    }
+    if (FD_ISSET(server_socket, &read_fds)) {
+      char buf[1025];
+
+
+    }
+
+  }
+
+
+
+}
+
+
+
+
+
